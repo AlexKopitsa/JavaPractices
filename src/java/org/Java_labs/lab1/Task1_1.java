f44bba63434d4bd9eb0af1e1568cc9315cfd8818
Oleksii <akapitsa117@gmail.com>
2022-10-09 20:50:32 +0300

    deleted files

--- src/java/org/Java_labs/lab1/Task1_1.java
+++
@@ -1,8 +0,0 @@
-package org.javapractices.lab1;
-
-public class Task1_1 {
-	public static void main(String[] args) {
-		System.out.println(new Task1_1SupportClass().toString());
-	}
-
-}
--- src/java/org/Java_labs/lab1/Task1_8.java
+++
@@ -1,7 +0,0 @@
-package org.javapractices.lab1;
-
-public class Task1_8 {
-    public static void main(String[] args) {
-        System.out.println("Це задачка 1.8");
-    }
-}
