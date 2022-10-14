59efac7fc48c86381a149e823b0e2f158e107d98
Oleksii <akapitsa117@gmail.com>
2022-10-14 23:14:13 +0300

    del

--- src/java/org/Java_labs/lab1/Task1_5.java
+++
@@ -1,9 +0,0 @@
-package org.Java_labs.lab1;
-
-public class Task1_5 {
-    public static void main(String[] args) {
-        for (String str : args) {
-            System.out.println("Argument " + str);
-        }
-    }
-}
