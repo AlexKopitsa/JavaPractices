48925a18b796800abbf1374ef643058a609ac917
Oleksii <akapitsa117@gmail.com>
2022-11-05 23:58:29 +0200

    s

--- src/java/org/Java_labs/lab6/Task1_1Specimen.java
+++
@@ -1,8 +0,0 @@
-package org.Java_labs.lab6;
-import org.Java_labs.lab6.Task1_1Packet.Task1_1;
-
-public class Task1_1Speciman {
-    public static void main(String[] args) {
-        Task1_1 task1_1 = new Task1_1();
-    }
-}
